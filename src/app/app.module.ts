9fcc131d6ea15416fc9e854f5751531749cec8ab
PerezBruno <brunoleandroperez@hotmail.com>
2022-09-12 04:06:48 -0300

    primer commit con componentes iniciales... arrancando de cero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactSectionComponent } from './components/contact-section/contact-section.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FreelancerAvalilableComponent } from './components/freelancer-avalilable/freelancer-avalilable.component';
+import { HomeSectionComponent } from './components/home-section/home-section.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PortfolioSectionComponent } from './components/portfolio-section/portfolio-section.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactSectionComponent,
+    ExperienceComponent,
+    FooterComponent,
+    FreelancerAvalilableComponent,
+    HomeSectionComponent,
+    NavbarComponent,
+    PortfolioSectionComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/freelancer-avalilable/freelancer-avalilable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-freelancer-avalilable',
+  templateUrl: './freelancer-avalilable.component.html',
+  styleUrls: ['./freelancer-avalilable.component.css']
+})
+export class FreelancerAvalilableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
