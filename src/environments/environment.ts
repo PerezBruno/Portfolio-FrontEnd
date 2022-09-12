bbca30c5687e63a9c3e55bcd7609e2c138850e0a
PerezBruno <brunoleandroperez@hotmail.com>
2022-09-12 05:23:07 -0300

    firebase instalado y hosting operativo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDAUDTMPcH_oNXDyHyUoceT1op4LkMIqRU",
+    authDomain: "perezbrunocv-510f8.firebaseapp.com",
+    projectId: "perezbrunocv-510f8",
+    storageBucket: "perezbrunocv-510f8.appspot.com",
+    messagingSenderId: "858991237852",
+    appId: "1:858991237852:web:e92cef6c4de5e98825b219"
+  }
 };
 
 /*
